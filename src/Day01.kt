33f4b40e9e7b075e73675328ccf73432fac82b19
SHARATH MAHADEV BHAT <sharubhat@gmail.com>
2023-12-02 22:41:01 -0800

    First day solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val digits = mapOf(
+        "zero" to 0,
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun getCalibrationValue(it: String) =
+        "${it.first { it.isDigit() }}${it.last() { it.isDigit() }}".toInt()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun String.possibleWordsAt(startingAt: Int): List<String> =
+        (3..5).map {
+            substring(startingAt, (startingAt + it).coerceAtMost(length))
+        }
+
+    fun getCalibrationValueProb2(line: String): Int {
+        val res = getCalibrationValue(
+            line.mapIndexedNotNull { index, c ->
+                when {
+                    c.isDigit() -> c.digitToInt()
+                    else -> line.possibleWordsAt(index).firstNotNullOfOrNull { digits[it] }
+                }
+            }.joinToString()
+        )
+        "$line -> $res".println()
+        return res
+    }
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    input.map { getCalibrationValue(it) }.foldRight(0) { x, y -> x + y }.println()
+    input.map { getCalibrationValueProb2(it) }.foldRight(0) { x, y -> x + y }.println()
 }
